e02e855f778e66e5284391e77e76a71eff7bf9f6
András Fehér <andras.feher@gmail.com>
2016-09-15 10:49:26 +0200

    Add files via upload

---
+++ Learning-and-Understanding/wf/js/directives.js
@@ -0,0 +1,14 @@
+//DIRECTIVES
+weatherApp.directive("weatherReport", function(){
+    return {
+	restrict: 'E',
+	templateUrl: 'js/directives/weatherReport.html',
+	replace: true,
+	scope: {
+	    weatherDay: "=",
+	    convertToStandard: "&",
+	    convertToDate: "&",
+	    dateFormat: "@"
+	}
+    }
+});
